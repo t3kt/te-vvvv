9641808b82bd35746abb5b67d14b5a58fc9df7a7
tommy.etkin <tommy.etkin@d83ba12b-2082-b1e8-93cf-1cdae69119af>
2011-02-25 03:17:30 +0000

    most of re-implementation of StructTypeRegistry done (unused deprecated implementations still there)

---
+++ trunk/plugins/StructNodes/Lib/RefTracker.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VVVV.Lib
+{
+
+	#region RefTracker<T>
+
+	internal sealed class RefTracker<T>
+	{
+
+		#region Static / Constant
+
+		#endregion
+
+		#region Fields
+
+		private readonly T _Item;
+		private int _RefCount;
+
+		#endregion
+
+		#region Properties
+
+		public int RefCount
+		{
+			get { return this._RefCount; }
+		}
+
+		#endregion
+
+		#region Events
+
+		public event EventHandler Emptied;
+
+		#endregion
+
+		#region Constructors
+
+		public RefTracker(T item)
+		{
+			this._Item = item;
+		}
+
+		#endregion
+
+		#region Methods
+
+		public T RequestRef()
+		{
+			_RefCount++;
+			return _Item;
+		}
+
+		public bool ReleaseRef()
+		{
+			_RefCount--;
+			if(_RefCount > 0)
+				return false;
+			var handler = this.Emptied;
+			if(handler != null)
+				handler(this, EventArgs.Empty);
+			return true;
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
