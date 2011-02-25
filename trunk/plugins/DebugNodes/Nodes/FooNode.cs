6fbc66994d4b9c952d7ec8860342479024d97e05
tommy.etkin <tommy.etkin@d83ba12b-2082-b1e8-93cf-1cdae69119af>
2011-02-25 00:59:00 +0000

    Created basic StructNodes type display GUI plugin, preparing to switch StructTypeRegistry to alternate implementation, misc other stuff in StructNodes. also some work on DebugNodes.

---
+++ trunk/plugins/DebugNodes/Nodes/FooNode.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes
+{
+
+	#region FooNode
+
+	[PluginInfo(Name = "Foo", Category = "Debug", Author = "te")]
+	public sealed class FooNode : IPluginEvaluate
+	{
+
+		#region Static / Constant
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Constructors
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+		#region IPluginEvaluate Members
+
+		public void Evaluate(int spreadMax)
+		{
+		}
+
+		#endregion
+	}
+
+	#endregion
+
+}
---
+++ trunk/plugins/DebugNodes/Nodes/GetAppDomainAssembliesNode.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes
+{
+
+	#region GetAppDomainAssembliesNode
+
+	[PluginInfo(Name = "Assemblies",
+		Category = "Debug",
+		Author = "te")]
+	public sealed class GetAppDomainAssembliesNode : IPluginEvaluate
+	{
+
+		#region Static / Constant
+
+		#endregion
+
+		#region Fields
+
+		[Input("Refresh", IsSingle = true, IsBang = true)]
+		private IDiffSpread<bool> _RefreshInput;
+
+		[Output("FullNames")]
+		private ISpread<string> _FullNameOutput;
+
+		[Output("Locations")]
+		private ISpread<string> _LocationOutput;
+
+		[Output("CodeBases")]
+		private ISpread<string> _CodeBaseOutput;
+
+		[Output("Assemblies", Visibility = PinVisibility.OnlyInspector)]
+		private ISpread<Assembly> _AssemblyOutput;
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Constructors
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+		#region IPluginEvaluate Members
+
+		public void Evaluate(int spreadMax)
+		{
+			if(_RefreshInput.IsChanged)
+			{
+				var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+					.Where(a => !(a.ManifestModule is ModuleBuilder))
+					.OrderBy(a => a.FullName, StringComparer.OrdinalIgnoreCase)
+					.ToArray();
+				_FullNameOutput.SliceCount =
+					_LocationOutput.SliceCount =
+					_CodeBaseOutput.SliceCount =
+					_AssemblyOutput.SliceCount =
+					assemblies.Length;
+				for(var i = 0; i < assemblies.Length; i++)
+				{
+					_FullNameOutput[i] = assemblies[i].FullName;
+					_LocationOutput[i] = assemblies[i].Location;
+					_CodeBaseOutput[i] = assemblies[i].CodeBase;
+					_AssemblyOutput[i] = assemblies[i];
+				}
+			}
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
