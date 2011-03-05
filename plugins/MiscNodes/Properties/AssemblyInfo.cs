992db6831359eeec3e494351082a90c2a48da8ff
tommy.etkin <tommy.etkin@d83ba12b-2082-b1e8-93cf-1cdae69119af>
2011-03-05 00:44:16 +0000

    created MiscNodes project for miscellaneous nodes.. LastChanged nodes work.

---
+++ plugins/MiscNodes/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MiscNodes")]
+[assembly: AssemblyDescription("Misc VVVV Nodes")]
+[assembly: AssemblyProduct("MiscNodes")]
+
+[assembly: Guid("845bba34-b537-4e40-a962-22e78410b65a")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
