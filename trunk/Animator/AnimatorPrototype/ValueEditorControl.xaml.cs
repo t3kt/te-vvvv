a4539fd6fb4dbf3cc0f55c85192a0312286c0bf2
tommy.etkin <tommy.etkin@d83ba12b-2082-b1e8-93cf-1cdae69119af>
2011-04-14 01:49:46 +0000

    misc ui stuff... nothing especially significant

---
+++ trunk/Animator/AnimatorPrototype/ValueEditorControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimatorPrototype
+{
+	/// <summary>
+	/// Interaction logic for ValueEditorControl.xaml
+	/// </summary>
+	public partial class ValueEditorControl : UserControl
+	{
+		public ValueEditorControl()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
