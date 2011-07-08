b99750f7c3d164812373b73bb4d31651d42c80bd
tommy.etkin <tommy.etkin@d83ba12b-2082-b1e8-93cf-1cdae69119af>
2011-07-08 01:55:13 +0000

    some work on the sequencing UI, and some misc other UI stuff

---
+++ trunk/Animator/src/ani/UI/Panes/SectionEditorPane.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Animator.Core.Model.Sequences;
+using Animator.Core.Model.Sessions;
+
+namespace Animator.UI.Panes
+{
+	/// <summary>
+	/// Interaction logic for SectionEditorPane.xaml
+	/// </summary>
+	public partial class SectionEditorPane
+	{
+
+		#region Static / Constant
+
+		static SectionEditorPane()
+		{
+			//DataContextProperty.OverrideMetadata(typeof(SectionEditorPane),
+			//    new PropertyMetadata(null, OnDataContextChanged));
+			
+		}
+
+		public static readonly DependencyProperty SequenceEditorProperty =
+			DependencyProperty.Register("SequenceEditor", typeof(object), typeof(SectionEditorPane),
+			new PropertyMetadata(null, OnEditorChanged));
+
+		public static readonly DependencyProperty SessionEditorProperty =
+			DependencyProperty.Register("SessionEditor", typeof(object), typeof(SectionEditorPane),
+			new PropertyMetadata(null, OnEditorChanged));
+
+		//private static void OnDataContextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		//{
+		//    ((SectionEditorPane)d).UpdateEditors();
+		//}
+
+		private static void OnEditorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			((SectionEditorPane)d).UpdateEditors();
+		}
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+
+		#region Properties
+
+		public object SequenceEditor
+		{
+			get { return this.GetValue(SequenceEditorProperty); }
+			set { this.SetValue(SequenceEditorProperty, value); }
+		}
+
+		public object SessionEditor
+		{
+			get { return this.GetValue(SessionEditorProperty); }
+			set { this.SetValue(SessionEditorProperty, value); }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public SectionEditorPane()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Methods
+
+		private void UpdateEditors()
+		{
+			var type = this.DataContext == null ? null : this.DataContext.GetType();
+			if(type == null)
+				this.Content = null;
+			else if(type == typeof(Sequence))
+				this.Content = this.SequenceEditor;
+			else if(type == typeof(Session))
+				this.Content = this.SessionEditor;
+		}
+
+		private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			this.UpdateEditors();
+		}
+
+		#endregion
+
+	}
+
+}
