51346413fe15993fc8f099601b6d9548323eae3b
tommy.etkin <tommy.etkin@d83ba12b-2082-b1e8-93cf-1cdae69119af>
2011-07-09 19:10:19 +0000

    misc UI stuff... including a really useful general purpose value converter (SwitchConverter)

---
+++ Animator/src/ani/UI/Common/SwitchConverter.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Animator.UI.Common
+{
+
+	#region SwitchCaseBase
+
+	public abstract class SwitchCaseBase
+	{
+
+		#region Static / Constant
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+
+		#region Properties
+
+		public object Value { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		#endregion
+
+		#region Methods
+
+		internal abstract bool TestMatch(object input);
+
+		#endregion
+
+	}
+
+	#endregion
+
+	#region SwitchCase
+
+	public class SwitchCase : SwitchCaseBase
+	{
+
+		#region Static / Constant
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+
+		#region Properties
+
+		public object Test { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		#endregion
+
+		#region Methods
+
+		internal override bool TestMatch(object input)
+		{
+			return Object.Equals(input, this.Test);
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+	#region TypeCase
+
+	public sealed class TypeCase : SwitchCaseBase
+	{
+
+		#region Static / Constant
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+
+		#region Properties
+
+		public Type Type { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		#endregion
+
+		#region Methods
+
+		internal override bool TestMatch(object input)
+		{
+			return this.Type != null && this.Type.IsInstanceOfType(input);
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+	#region SwitchConverter
+
+	[ContentProperty("Cases")]
+	public class SwitchConverter : IValueConverter
+	{
+
+		#region Static / Constant
+
+		#endregion
+
+		#region Fields
+
+		private readonly List<SwitchCaseBase> _Cases = new List<SwitchCaseBase>();
+
+		#endregion
+
+		#region Properties
+
+		public List<SwitchCaseBase> Cases
+		{
+			get { return this._Cases; }
+		}
+
+		public object DefaultValue { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+		#region IValueConverter Members
+
+		public object Convert(object input, Type targetType, object parameter, CultureInfo culture)
+		{
+			foreach(var c in this._Cases)
+			{
+				if(c.TestMatch(input))
+					return c.Value;
+			}
+			return this.DefaultValue;
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
