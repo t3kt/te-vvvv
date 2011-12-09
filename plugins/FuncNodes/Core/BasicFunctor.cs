b49a7e949146ab9fb9d14da9137db5241a38c8dc
tommy.etkin@gmail.com <tommy.etkin@gmail.com@d83ba12b-2082-b1e8-93cf-1cdae69119af>
2011-12-09 03:29:26 +0000

    started FuncNodes for using function programming concepts in vvvv

---
+++ plugins/FuncNodes/Core/BasicFunctor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FuncNodes.Core
+{
+
+	#region BasicFunctor
+
+	internal sealed class BasicFunctor : IFunctor
+	{
+
+		#region Static/Constant
+
+		#endregion
+
+		#region Fields
+
+		private readonly Func<object[], object[]> _Evaluate;
+		private readonly int _ArgCount;
+		private readonly int _ResultCount;
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Constructors
+
+		public BasicFunctor(Func<object[], object[]> evaluate, int argCount, int resultCount = 1)
+		{
+			this._Evaluate = evaluate;
+			this._ArgCount = argCount;
+			this._ResultCount = resultCount;
+		}
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+		#region IFunctor Members
+
+		public int ArgCount
+		{
+			get { return _ArgCount; }
+		}
+
+		public int ResultCount
+		{
+			get { return _ResultCount; }
+		}
+
+		public object[] Evaluate(object[] args)
+		{
+			return _Evaluate(args);
+		}
+
+		#endregion
+	}
+
+	#endregion
+
+}
---
+++ plugins/FuncNodes/Nodes/AddFuncNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using FuncNodes.Core;
+using VVVV.PluginInterfaces.V2;
+
+namespace FuncNodes.Nodes
+{
+
+	#region Add2FuncNode<T>
+
+	public abstract class Add2FuncNode<T> : IPartImportsSatisfiedNotification
+	{
+
+		#region Static/Constant
+
+		#endregion
+
+		#region Fields
+
+		private IFunctor _Functor;
+
+		[Output("Function", IsSingle = true)]
+		private ISpread<IFunctor> _FuncOutput;
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Constructors
+
+		#endregion
+
+		#region Methods
+
+		protected abstract T Add(T arg1, T arg2);
+
+		#endregion
+
+		#region IPartImportsSatisfiedNotification Members
+
+		public void OnImportsSatisfied()
+		{
+			//_Functor=new BasicFunctor((args)=>);
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+
+	#endregion
+
+}
