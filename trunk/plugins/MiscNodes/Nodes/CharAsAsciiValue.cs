e2e1aa3565480b60a0fd81607a5658171e3bd98e
tommy.etkin@gmail.com <tommy.etkin@gmail.com@d83ba12b-2082-b1e8-93cf-1cdae69119af>
2011-12-31 15:07:17 +0000

    removed deprecated old plugins code (and migrated the "AsciiChar (String)" node to MiscNodes), also some minor stuff in qw2

--- trunk/plugins/MiscNodes/Nodes/CharAsAsciiValue.cs
+++ trunk/plugins/MiscNodes/Nodes/CharAsAsciiValue.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -60,5 +59,4 @@ namespace VVVV.Nodes
 	}
 
 	#endregion
-
 }
